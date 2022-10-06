3fd2b9ac9c77c187f3ae373a326cb795335bd6ee
jesusCampos03 <chucm12@gmail.com>
2022-10-05 20:43:29 -0600

    diseño 3

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.lugares
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.lugares.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        binding = activityMainBingding.inflate(layoutInflater)
+        setContentView(binding.root)}
+        firebaseApp.initializeAPP(this)
+    auth = firebase.outh
+}
+
+private fun hacerlogin (){
+    var email = binding.etmail.text.tostring()
+
 }
